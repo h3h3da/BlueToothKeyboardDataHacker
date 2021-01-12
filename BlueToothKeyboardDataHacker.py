6782761610b1ecb3cca26c84f99a4b1da0119280
h3h3da <1162858612@qq.com>
2021-01-12 13:13:48 +0800

    init

---
+++ BlueToothKeyboardDataHacker.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+from scapy.all import *
+#from scapy import *
+
+presses = []
+
+normalKeys = {"04":"a", "05":"b", "06":"c", "07":"d", "08":"e", "09":"f", "0a":"g", "0b":"h", "0c":"i", "0d":"j", "0e":"k", "0f":"l", "10":"m", "11":"n", "12":"o", "13":"p", "14":"q", "15":"r", "16":"s", "17":"t", "18":"u", "19":"v", "1a":"w", "1b":"x", "1c":"y", "1d":"z","1e":"1", "1f":"2", "20":"3", "21":"4", "22":"5", "23":"6","24":"7","25":"8","26":"9","27":"0","28":"<RET>","29":"<ESC>","2a":"<DEL>", "2b":"\t","2c":"<SPACE>","2d":"-","2e":"=","2f":"[","30":"]","31":"\\","32":"<NON>","33":";","34":"'","35":"<GA>","36":",","37":".","38":"/","39":"<CAP>","3a":"<F1>","3b":"<F2>", "3c":"<F3>","3d":"<F4>","3e":"<F5>","3f":"<F6>","40":"<F7>","41":"<F8>","42":"<F9>","43":"<F10>","44":"<F11>","45":"<F12>"}
+
+shiftKeys = {"04":"A", "05":"B", "06":"C", "07":"D", "08":"E", "09":"F", "0a":"G", "0b":"H", "0c":"I", "0d":"J", "0e":"K", "0f":"L", "10":"M", "11":"N", "12":"O", "13":"P", "14":"Q", "15":"R", "16":"S", "17":"T", "18":"U", "19":"V", "1a":"W", "1b":"X", "1c":"Y", "1d":"Z","1e":"!", "1f":"@", "20":"#", "21":"$", "22":"%", "23":"^","24":"&","25":"*","26":"(","27":")","28":"<RET>","29":"<ESC>","2a":"<DEL>", "2b":"\t","2c":"<SPACE>","2d":"_","2e":"+","2f":"{","30":"}","31":"|","32":"<NON>","33":"\"","34":":","35":"<GA>","36":"<","37":">","38":"?","39":"<CAP>","3a":"<F1>","3b":"<F2>", "3c":"<F3>","3d":"<F4>","3e":"<F5>","3f":"<F6>","40":"<F7>","41":"<F8>","42":"<F9>","43":"<F10>","44":"<F11>","45":"<F12>"}
+
+def main():
+    
+    packets = rdpcap("so_easy.pcap")
+
+    DataFileName = "usbdata_test"
+
+    count=0
+
+    for p in packets:
+        # 定位 HCI USB
+        if len(p) == 35:
+            print(bytes(p).hex()[-16:])
+            f = open(DataFileName, 'a')
+            f.write(bytes(p).hex()[-16:] + '\n')
+            f.close()
+            count += 1
+
+    print(count)
+    
+    # read data
+    with open(DataFileName, "r") as f:
+        for line in f:
+            presses.append(line[0:-1])
+    # handle
+    result = ""
+    for press in presses:
+        if press == '':
+            continue
+        if ':' in press:
+            Bytes = press.split(":")
+        else:
+            Bytes = [press[i:i+2] for i in range(0, len(press), 2)]
+        if Bytes[0] == "00":
+            if Bytes[2] != "00" and normalKeys.get(Bytes[2]):
+                result += normalKeys[Bytes[2]]
+        elif int(Bytes[0],16) & 0b10 or int(Bytes[0],16) & 0b100000: # shift key is pressed.
+            if Bytes[2] != "00" and normalKeys.get(Bytes[2]):
+                result += shiftKeys[Bytes[2]]
+        else:
+            print("[-] Unknow Key : %s" % (Bytes[0]))
+    # print("[+] Found : %s" % (result))
+    print("[+] Found : %s" % (result.replace("<SPACE>", " ").replace("<RET>", "\n").replace("<GA>","")))    
+
+    # clean the temp data
+    # os.system("rm ./%s" % (DataFileName))
+    
+    
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # BlueToothKeyboardDataHacker
 蓝牙键盘流量包取证工具 , 用于恢复用户的击键信息
+
+## Usage
+
+把usb流量包保存为so_easy.pcap（或者自定义修改下代码就好了）。
+执行：
+```
+python3 BlueToothKeyboardDataHacker.py
+```
+
